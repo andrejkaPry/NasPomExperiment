cde719b8cbc1669be8655bd1a5acac76393a30e7
Andrea Pryglova <andy@gmail.com>
2020-11-12 20:18:38 +0100

    Initial commit

---
+++ src/main/java/cz/czechitas/SpousteciTrida.java
@@ -0,0 +1,12 @@
+package cz.czechitas;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.*;
+
+public class SpousteciTrida {
+
+    public static void main(String[] args) {
+        System.out.println("Zdravim, pozemstani! Zavedte me ke svemu vudci");
+        WebDriver browser = new FirefoxDriver();
+    }
+}
